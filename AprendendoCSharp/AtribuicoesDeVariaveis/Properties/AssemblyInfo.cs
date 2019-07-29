3013125102af08614c8b1122fd97396b3be57452
Gabriel Brum <gabrielbrum7@gmail.com>
2019-07-29 19:15:09 -0300

    Terceiro commit, adicionando mais conhecimentos de C#.

---
+++ AprendendoCSharp/AtribuicoesDeVariaveis/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("AtribuicoesDeVariaveis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AtribuicoesDeVariaveis")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("ee81a4cb-8451-4737-8769-8a9939f062e3")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AprendendoCSharp/ExercicioCondicional2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioCondicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execício de Condicionais Booleana");
+
+            int idade = 68;
+            bool ehIdoso = idade >= 65;
+
+            Console.WriteLine(ehIdoso);
+
+            Console.ReadLine();
+        }
+    }
+}
