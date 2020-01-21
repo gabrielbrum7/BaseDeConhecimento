94a1f0004f163c68606bb55f8dfe7bd01ea03db2
Gabriel Brum <53318145+gabrielbrum7@users.noreply.github.com>
2020-01-20 22:21:20 -0300

    Add files via upload
    Adicionando conhecimentos de PHP.

---
+++ AprendendoPHP.php
@@ -0,0 +1,36 @@
+Aprendendo PHP
+
+http://aprenderphp.com.br/artigo/comecando-a-programar-em-php/
+
+-Delimitadores de código, é a tag de abertura e fechamento
+
+<?php
+
+// Código PHP aqui!
+
+?>
+
+-O arquivo deve ser salvo em ".php"
+
+-O PHP pode ser misturado com o HTML
+
+Variáveis
+
+-A representação de variável é feita por um "$" seguido pelo nome da variável
+
+	-É Case Sensitive
+	-Nomes de variável deve ser iniciado com letra ou sublinhado"_". Números podem aparecer em outras posições, exceto na primeira
+	-Não pode ser usado caracteres especiais nas variáveis, nem acento.
+
+Exemplos de variáveis
+
+<?php
+
+$nome = 'Mauro George';
+$nascimento = '06/09/1989';
+$sobre_nome = 'Oliveira Tavares';
+
+?>
+
+-O ";" é o separador de instruções
+
