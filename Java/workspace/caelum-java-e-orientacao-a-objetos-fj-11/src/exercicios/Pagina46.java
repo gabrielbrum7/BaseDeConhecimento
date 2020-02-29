313c2c1dba58e1993594a316f524f9df70b36a98
gabrielbrum7 <gabrielbrum7@gmail.com>
2020-02-29 16:01:55 -0300

    Adicionando mais conhecimentos de Java.

--- Java/workspace/caelum-java-e-orientacao-a-objetos-fj-11/src/exercicios/Pagina46.java
+++ Java/workspace/caelum-java-e-orientacao-a-objetos-fj-11/src/exercicios/Pagina46.java
@@ -4,32 +4,41 @@ public class Pagina46 {
 
 	public static void main (String[] args) {
 		
-		// Exercícios da página 46
+		/* Exercícios da página 46 
+		
+		// Para ver o resultado de cada, descomente-os.
 		
 		// 1) Imprima todos os números de 150 a 300.
 		
-		//for (int i= 150; i >= 150 && i <= 300; i++) {
-		//	System.out.println(i);
-		//}
+		for (int i= 150; i >= 150 && i <= 300; i++) {
+			System.out.println(i);
+		}
 		
-		// 2) Imprima a soma de 1 até 1000.
+		*/
 		
-		int j = 1;
+		/* 2) Imprima a soma de 1 até 1000.
+		
 		int y = 0;
 		int l = 0;
 		
-		for (int i = 1; i <= 2; i++) {
-			int k = 0;
-			k = j + i;
-			System.out.println(k);
-			j++;	
-		}
-		for (int x = 8; x >= 8 && x <= 1000; x++) {
+		for (int x = 2; x >= 2 && x <= 1000; x++) {
 			y = x + l;
 			System.out.println(y);
 			l *= l;
 			x *= 2;
 			--x;
 		}
+		
+		*/
+		
+		/* 3) Imprima todos os múltiplos de 3, de 1 a 100.
+		
+		for (int i = 1; i <= 100; i++) {
+			if (3 % i == 0) {
+				System.out.println("Este é um múltiplo de 3: " + i);
+			} 			
+			
+		}*/
+						
 	}
 }
